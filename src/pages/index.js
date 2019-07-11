113932b0b7d8ce6d6f7bfb304741623c71905f1c
Gus Fune <gustavofunee@gmail.com>
2019-07-11 20:32:24 -0300

    Modal with contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,57 +2,105 @@ import React from "react"
 import { Link } from "gatsby"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
+import ReactModal from 'react-modal';
+import "./index.css"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <p>Estamos indo embora do país e vamos vender várias coisas que temos em casa. Aqui estão os itens já catalogados. Em breve vamos adicionar mais coisas.</p>
-    <p>Interessado em algo? Manda um whatsapp pra <a href="tel:+5561982109400">(61) 98210-9400</a>.</p>
-    <Tabs>
-      <TabList>
-        <Tab>Eletrônicos & Eletrodomésticos</Tab>
-        <Tab>Móveis</Tab>
-        <Tab>Diversos</Tab>
-      </TabList>
+class IndexPage extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      showModal: false
+    };
+    
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+  
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+  
+  handleCloseModal () {
+    this.setState({ showModal: false });
+  }
+  render() {
+    return(
+      <Layout>
+        <SEO title="Home" />
+        <p>Estamos indo embora do país e vamos vender várias coisas que temos em casa. Aqui estão os itens já catalogados. Em breve vamos adicionar mais coisas.</p>
+        <p>Interessado em algo? <a href="#" onClick={this.handleOpenModal}>Clique aqui</a>, preenche o formulário em um minuto e retornaremos em breve.</p>
 
-    <TabPanel>
-      <iframe
-        class="airtable-embed" 
-        src="https://airtable.com/embed/shrHykWNSJy1HhZQS?backgroundColor=purple&viewControls=on"
-        frameborder="0"
-        onmousewheel=""
-        width="100%"
-        height="533"
-        style={{ background: `transparent`, border: `1px solid #ccc` }}
-      />
-    </TabPanel>
-    <TabPanel>
-      <iframe
-        class="airtable-embed" 
-        src="https://airtable.com/embed/shrPQlNvbmpGXAEeM?backgroundColor=purple&viewControls=on"
-        frameborder="0"
-        onmousewheel=""
-        width="100%"
-        height="533"
-        style={{ background: `transparent`, border: `1px solid #ccc` }}
-      />
-    </TabPanel>
-    <TabPanel>
-      <iframe
-        class="airtable-embed" 
-        src="https://airtable.com/embed/shrEB5MVwvtWZAhjv?backgroundColor=purple&viewControls=on"
-        frameborder="0"
-        onmousewheel=""
-        width="100%"
-        height="533"
-        style={{ background: `transparent`, border: `1px solid #ccc` }}
-      />
-    </TabPanel>
-  </Tabs>
-  </Layout>
-)
+        <ReactModal
+          isOpen={this.state.showModal}
+          style={{
+            overflow: `hidden`
+          }}
+        >
+          <iframe
+            class="airtable-embed" 
+            src="https://airtable.com/embed/shrYnhFtNTXu5GiTC?backgroundColor=purple"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            height="100%"
+            style={{ 
+              background: `transparent`, 
+              height: `100%`,
+              overflow: `hidden`
+            }}
+          />
+          <p className="close">
+            <a href="#" onClick={this.handleCloseModal}>Fechar [x]</a>
+          </p>
+        </ReactModal>
+
+        <Tabs>
+          <TabList>
+            <Tab>Eletrônicos & Eletrodomésticos</Tab>
+            <Tab>Móveis</Tab>
+            <Tab>Diversos</Tab>
+          </TabList>
+
+        <TabPanel>
+          <iframe
+            class="airtable-embed" 
+            src="https://airtable.com/embed/shrHykWNSJy1HhZQS?backgroundColor=purple&viewControls=on"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            height="533"
+            style={{ background: `transparent`, border: `1px solid #ccc` }}
+          />
+        </TabPanel>
+        <TabPanel>
+          <iframe
+            class="airtable-embed" 
+            src="https://airtable.com/embed/shrPQlNvbmpGXAEeM?backgroundColor=purple&viewControls=on"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            height="533"
+            style={{ background: `transparent`, border: `1px solid #ccc` }}
+          />
+        </TabPanel>
+        <TabPanel>
+          <iframe
+            class="airtable-embed" 
+            src="https://airtable.com/embed/shrEB5MVwvtWZAhjv?backgroundColor=purple&viewControls=on"
+            frameborder="0"
+            onmousewheel=""
+            width="100%"
+            height="533"
+            style={{ background: `transparent`, border: `1px solid #ccc` }}
+          />
+        </TabPanel>
+      </Tabs>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage

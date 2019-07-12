2ff61410c4a3dea80030fa4fcc38c7175c9ac38f
Gus Fune <gustavofunee@gmail.com>
2019-07-12 00:44:29 -0300

    Added Analytics Tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,12 @@ module.exports = {
         icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-7103855-5"
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
